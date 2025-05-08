85c74849934fdaf11946676138528d739325d793
SamEdstrand <sedstrand@unomaha.edu>
2025-05-07 19:19:25 -0500

    Change

--- Project.py
+++ Project.py
@@ -3,8 +3,22 @@ import streamlit as st
 
 data = pd.read_csv('https://raw.githubusercontent.com/SamEdstrand/ECON-8320-Semester-Project/refs/heads/main/Data%20-%202.csv')
 
-#print(data['Gender'].sample(10))
+#remove unnecessary white spaces
+for col in data.select_dtypes(include='object').columns:
+    data[col] = data[col].str.strip()
 
-st.write(data.head(25))
+# clean city data
+for i, row in data.iterrows():
+    city = row['Pt City']
+    if city == '' or type(city) != str:
+        city = "Missing"
+    city = str(city)
+    city = city.lower()
+    city = city.title()
+    data.at[i, 'Pt City'] = city
+
+#print(data['Pt City'].sample(10))
+
+#st.write(data.head(25))
 
 
