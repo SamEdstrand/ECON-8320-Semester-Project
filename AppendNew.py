d43e4f436d6ed53e2d63dc69fbd0caa17f8e6af7
SamEdstrand <sedstrand@unomaha.edu>
2025-05-12 12:12:24 -0500

    Change

--- AppendNew.py
+++ AppendNew.py
@@ -2,7 +2,7 @@ import pandas as pd
 from DataClean_Function import clean
 from DataClean import data
 
-file_name = input("Enter file name: ")
+file_name = input("Enter csv file name: ")
 
 new_data = pd.read_csv(file_name)
 new_data = clean(new_data)
