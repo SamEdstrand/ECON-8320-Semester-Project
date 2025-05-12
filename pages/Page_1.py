33e6bd47dd1c99eeab7879b19181552449e0cc6e
SamEdstrand <sedstrand@unomaha.edu>
2025-05-11 21:00:36 -0500

    Change

--- pages/Page_1.py
+++ pages/Page_1.py
@@ -3,8 +3,7 @@ from DataClean import data
 
 st_data =  data
 
-#st.title("Applications for Review")
-st.set_page_config(page_title="Applications for Review", layout="wide", page_icon= "Requests")
+st.title("Applications for Review")
 
 
 need_review = st_data[st_data['Request Status'] == "Pending"]
