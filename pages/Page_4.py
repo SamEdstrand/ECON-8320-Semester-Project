d2871d3c64a25281ed830bdd38d14cdaa50c440b
SamEdstrand <sedstrand@unomaha.edu>
2025-05-12 12:34:35 -0500

    Change

--- pages/Page_4.py
+++ pages/Page_4.py
@@ -26,4 +26,4 @@ st.bar_chart(year_by_year_df)
 total = 0
 for a in year:
     total += year[a]
-st.write("Total assistance provided: $", total)
+st.write("Total assistance provided: $", round(total,0))
